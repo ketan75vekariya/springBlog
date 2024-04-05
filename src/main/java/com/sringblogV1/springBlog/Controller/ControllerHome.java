2564712a63aac974e36ae12c27277ad737f7bb06
ketan75vekariya <ketan75vekariya@yahoo.com>
2024-04-05 15:51:54 +0100

    Themeleaf template added

--- src/main/java/com/sringblogV1/springBlog/Controller/ControllerHome.java
+++ src/main/java/com/sringblogV1/springBlog/Controller/ControllerHome.java
@@ -5,13 +5,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-
 @Controller
 public class ControllerHome {
  
- 
+  
+  // Controller method for the isActivePage logic (not mapped to any HTTP endpoint)
+  
+
  @GetMapping("/home")
   public String home(Model model){
+    model.addAttribute("currentPage", "home");
     return "home";
+  }
+  @GetMapping("/about")
+  public String about(Model model){
+    model.addAttribute("currentPage", "about");
+    return "about";
+  }
+  @GetMapping("/contact")
+  public String contact(Model model){
+    model.addAttribute("currentPage", "contact");
+    return "contact";
+  }
+  @GetMapping("/blogs")
+  public String blogs(Model model){
+    model.addAttribute("currentPage", "blog");
+    return "blogs";
+  }
+  @GetMapping("/blog-post")
+  public String blogPost(Model model){
+    model.addAttribute("currentPage", "blog");
+    return "blog_post";
   }
 }
