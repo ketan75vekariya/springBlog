e0e23098f72dc3ce974f7579d6259dbdee0092c2
ketan75vekariya <ketan75vekariya@yahoo.com>
2024-04-06 11:18:38 +0100

    Listed out blogs in view/blogs.html from database

--- src/main/java/com/sringblogV1/springBlog/Controller/ControllerHome.java
+++ src/main/java/com/sringblogV1/springBlog/Controller/ControllerHome.java
@@ -1,13 +1,21 @@
 package com.sringblogv1.springblog.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sringblogv1.springblog.model.Post;
+import com.sringblogv1.springblog.services.PostService;
 
 
 @Controller
 public class ControllerHome {
  
+  @Autowired
+  private PostService postService;
   
   // Controller method for the isActivePage logic (not mapped to any HTTP endpoint)
   
@@ -30,6 +38,16 @@ public class ControllerHome {
   @GetMapping("/blogs")
   public String blogs(Model model){
     model.addAttribute("currentPage", "blog");
+    List<Post> posts = postService.getAll();
+    // Check if there are any posts
+    if (!posts.isEmpty()) {
+      // Retrieve the first post
+      Post firstPost = posts.get(0);
+      
+      // Add the first post to the model
+      model.addAttribute("firstPost", firstPost);
+  }
+    model.addAttribute("posts", posts);
     return "view/blogs";
   }
   @GetMapping("/blog-post")
