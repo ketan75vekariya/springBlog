c33741896e829a5f21d0a9aabae27ab9bb26e81c
ketan75vekariya <ketan75vekariya@yahoo.com>
2024-04-06 11:38:11 +0100

    Added account table in database by creating account.java model and also established relation between post and account

---
+++ src/main/java/com/sringblogV1/springBlog/model/Account.java
@@ -0,0 +1,30 @@
+package com.sringblogv1.springblog.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Account {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private long id;
+
+  private String email;
+
+  private String firstname; 
+
+  @OneToMany(mappedBy = "account")
+  private List<Post> posts;
+}
--- src/main/java/com/sringblogV1/springBlog/model/Post.java
+++ src/main/java/com/sringblogV1/springBlog/model/Post.java
@@ -5,7 +5,10 @@ import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,7 +20,7 @@ import lombok.Setter;
 public class Post {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private long id;
 
   private String title;
@@ -26,4 +29,7 @@ public class Post {
   private String body;
 
   private LocalDateTime createdAt;
+  @ManyToOne
+  @JoinColumn(name = "account_id", referencedColumnName = "id", nullable = true)
+  private Account account;
 }
